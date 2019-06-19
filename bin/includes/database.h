0a3ec03cf3e506a86090fde4c50bbbb25f32a09f
ethan <18201521626@139.com>
2019-06-19 12:53:40 +0800

    Update database.h

--- bin/includes/database.h
+++ bin/includes/database.h
@@ -8,9 +8,12 @@
 using namespace std;
 const int NR=1000;
 const int SON=100;
+inline bool is_vowel(char x){
+	return (x=='a')||(x=='e')||(x=='i')||(x=='o')||(x=='u');
+}
 struct word_chain{
 	map<string,int> indexof;
-	string stringof[NR];
+	string stringof[NR],cur,last,read;
 	int last_part_index/*最后一个组的编号*/;
 	int** son;
 	int *son_t;
@@ -21,10 +24,10 @@ struct word_chain{
 		for(int i=0;i<NR;i++)
 			son[i]=new int[SON];
 		son_t=new int[NR];
-		last_part_index=0;
 		for(int i=0;i<NR;i++)
 			memset(son[i],-1,SON*sizeof(int));
 		memset(son_t,0,NR*sizeof(int));
+		last_part_index=0;
 		printf("Variable initialization completed.\n");
 		//memset(not_son,-1,sizeof(not_son));
 		//memset(not_son_t,0,sizeof(not_son_t));
@@ -64,6 +67,31 @@ struct word_chain{
 	//-----------------------------function using map end--------------------------------------
 	//第一个参数:统计并清除法编号为strnum的节点的子节点
 	//第二个参数:清除出现次数少于k的
+	void input(){
+		last="start";
+		cin>>read;
+		int t=0;
+		//每一部分循环一次
+		int len=(int)read.length();
+		while(t<len){
+			//分离元音组和辅音组
+			if(is_vowel(read[t])){
+				//记录该部分的开始下标
+				int savet=t;
+				while(is_vowel(read[t])&&t<len)t++;
+				cur.assign(read,savet,t-savet);
+			}
+			else{
+				//记录该部分的开始下标
+				int savet=t;
+				while(!is_vowel(read[t])&&t<len)t++;
+				cur.assign(read,savet,t-savet);
+			}
+			push_next(last,cur);
+			last=cur;
+		}
+		push_next(last,"end");
+	}
 	void clean(int strnum,int k){
 		sort(son[strnum]+1,son[strnum]+son_t[strnum]+1);
 		int cnt=0;
@@ -81,8 +109,10 @@ struct word_chain{
 		int index_of_done=1;
 		for(int i=1;i<=son_t[strnum];i++){
 			if(son[strnum][i]==-1)continue;
-			son[strnum][index_of_done]=son[strnum][i];
-			if(i!=index_of_done)son[strnum][i]=-1;
+			if(i!=index_of_done){
+				son[strnum][i]=-1;
+				son[strnum][index_of_done]=son[strnum][i];
+			}
 			index_of_done++;
 		}
 		son_t[strnum]=index_of_done-1;
