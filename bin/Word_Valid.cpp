e9537b48b66effc329c96b934670d84769438416
Edsger-Wybe-Dijkstra <40728083+Edsger-Wybe-Dijkstra@users.noreply.github.com>
2019-06-24 14:17:02 +0800

    added Trie to the file

--- bin/Word_Valid.cpp
+++ bin/Word_Valid.cpp
@@ -1,17 +1,52 @@
-// <README>
-// 此判断程序必须有10000单词的词库和此程序在同一目录下才可以正常运作
-// 且10000单词的词库必须被命名为1000-wbase.txt
-// <\README>
-
 #include <bits/stdc++.h>
 using namespace std;
 set<string> word_base_10000;
+struct Node {
+	char c;
+	Node* chd[26];
+	bool finish;
+	Node() {
+		c = '~';
+		memset(chd, NULL, sizeof(chd));
+		finish = 0;
+	}
+};
+struct Trie {
+	Node *_root;
+	Trie() {
+		_root = new Node;
+	}
+	void Insert(Node *&p, string s, int pos) {
+		if (p == NULL) {
+			p = new Node;
+			p->c = s[pos];
+		}
+		if (pos != s.length() - 1) {
+			Insert(p->chd[s[pos + 1] - 'a'], s, pos + 1);
+		} else {
+			p->finish = 1;
+		}
+	}
+	bool Query(Node *&p, string s, int pos) {
+		if (p == NULL) {
+			return 0;
+		}
+		if (pos != s.length() - 1) {
+			return Query(p->chd[s[pos + 1] - 'a'], s, pos + 1);
+		} else if (p->finish) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+};
+Trie tr;
 string s;
 void input() {
 	ifstream fin("1000-wbase.txt");
 	string s;
 	while(fin >> s) {
-		word_base_10000.insert(s);
+		tr.Insert(tr._root, s, -1);
 		// printf("Reading word\n");
 	}
 }
@@ -21,7 +56,7 @@ int main() {
 		cout << "Enter the word that you want to check." << endl;
 		cout << "Console >> ";
 		cin >> s;
-		if(word_base_10000.find(s) != word_base_10000.end()) {
+		if(tr.Query(tr._root, s, -1)) {
 			cout << "The word you entered is valid." << endl;
 		}
 		else {
