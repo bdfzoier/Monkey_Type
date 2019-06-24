29f78d372f66f203bde15788a070e587671caf6f
Edsger-Wybe-Dijkstra <40728083+Edsger-Wybe-Dijkstra@users.noreply.github.com>
2019-06-24 10:45:34 +0800

    Update randlist.h
    Add Getrand and randomprefix

--- bin/includes/randlist.h
+++ bin/includes/randlist.h
@@ -2,6 +2,7 @@
 #define DATABASE_H 1
 
 #include<algorithm>
+#include <ctime>
 
 #define NR 100000
 
@@ -14,6 +15,7 @@ struct arr_val{
 };
 struct prop_arr{
 	arr_val *l;
+	int lft[NR], rgt[NR], sum;
 	int t;
 	prop_arr(){
 		t=0;
@@ -32,10 +34,27 @@ struct prop_arr{
 			}
 		}
 	}
+	//use this before using GETRANDOM(), else the GETRANDOM function will run into problems.
+	arr_val RandomPrefix() {
+		rgt[0] = -1;
+		std::srand(unsigned(std::time(NULL)));
+		for (int i = 1;i <= t;i++) {
+			lft[i] = rgt[i - 1] + 1;
+			rgt[i] = lft[i] + l[t].prop - 1;
+			sum += l[t].prop;
+		}
+	}
+	arr_val GetRandom() {
+		int k = (rand() % 3000) * (rand() % 3000) % sum;
+		int idx = lower_bound(lft, lft + t, k) - l;
+		return l[idx];
+	}
 	void print(){
 		for(int i=1;i<=t;i++)
 			printf("%d,%d ",l[i].value,l[i].prop);
 	}
 };
+
+#undef NR
 
 #endif
